5a16e26f368f8ba3e5169036adf95a8ec756dd67
Colin Prepscius <colinprepscius@gmail.com>
2026-03-10 08:58:11 -0400

    update numpy, v 0.1.2

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ class CMakeBuild(build_ext):
             extdir += os.path.sep
 
         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+                      '-DPython_EXECUTABLE=' + sys.executable]
 
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
@@ -49,7 +49,7 @@ long_description = (Path(__file__).parent / "README.md").read_text()
 
 setup(
     name='roboflex.transport.zenoh',
-    version='0.0.1',
+    version='0.1.2',
     description='Roboflex Transport Zenoh Library',
     author='Colin Prepscius',
     author_email='colinprepscius@gmail.com',
@@ -70,7 +70,7 @@ setup(
     keywords = ["zenoh", "pubsub", "robotics", "middleware", "flexbuffers", "python", "c++", "c++20"],
     license = "MIT",
     python_requires='>=3.6',
-    install_requires=['numpy', 'roboflex'],
+    install_requires=['numpy>=2.0', 'roboflex'],
     ext_modules=[CMakeExtension('roboflex/transport/zenoh/roboflex_transport_zenoh_ext')],
     cmdclass=dict(build_ext=CMakeBuild),
     py_modules=['__init__'],
